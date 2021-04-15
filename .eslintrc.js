f1f9e1b4005962d95b96e70608e5e11346e45a80
shihao wang <shihao.wang@hand-china.com>
2021-04-15 17:24:49 +0800

    feat: 修改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0
+  },
 };
