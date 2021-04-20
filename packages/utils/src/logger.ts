49688c00138f2b1e4b4ceed766f0b9122b8469d7
shihao wang <shihao.wang@hand-china.com>
2021-04-20 17:41:07 +0800

    feat: 补充日志工具

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -1,19 +1,32 @@
 import chalk from 'chalk';
+import { isFirstWorker } from './common';
 
 export function createLogger(namespace: string) {
   return {
     debug: (...args: any[]) => {
       process.env.NODE_ENV === 'development' &&
-        console.log(`${chalk.redBright(namespace)}: `, ...args);
+        isFirstWorker() &&
+        console.log(`DEBUG (${chalk.redBright(namespace)}) : `, ...args);
     },
     info(...args: any[]) {
-      console.log(chalk.blueBright(`Info:`), ...args);
+      console.log(chalk.blueBright(`Info (${namespace}) :`), ...args);
+    },
+    infoOnly(...args: any[]) {
+      isFirstWorker() &&
+        console.log(chalk.blueBright(`Info (${namespace}) :`), ...args);
     },
     success(...args: any[]) {
       console.log(chalk.greenBright(...args));
     },
+    successOnly(...args: any[]) {
+      isFirstWorker() && console.log(chalk.greenBright(...args));
+    },
     error(...args: any[]) {
-      console.log(chalk.red(`Error:`), ...args);
+      console.log(chalk.red(`Error (${namespace}) :`), ...args);
+    },
+    errorOnly(...args: any[]) {
+      isFirstWorker() &&
+        console.log(chalk.red(`Error (${namespace}) : `), ...args);
     },
   };
 }
