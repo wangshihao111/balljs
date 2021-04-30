345b1866fd9de0e4fe36cfe413ac4653e3dec01e
shihao wang <913003120@qq.com>
2021-04-30 09:53:33 +0800

    feat：完善 build 命令

--- packages/cli/src/actions/build.ts
+++ packages/cli/src/actions/build.ts
@@ -1,10 +1,21 @@
 import { spawn } from 'child_process';
 import rimraf from 'rimraf';
+import fs from 'fs-extra';
+import globby from 'globby';
 import path from 'path';
 import { cwd } from '../utils';
 
+// TODO: 输出文件路径与输入文件路径应该可配
+
+const distPath = path.resolve(cwd, 'dist');
+
 export async function runBuild(): Promise<void> {
-  rimraf.sync(path.resolve(cwd, 'dist'), {});
+  await build();
+  await copyFiles();
+}
+
+async function build(): Promise<void> {
+  rimraf.sync(distPath, {});
   return new Promise((resolve, reject) => {
     const command = 'npx tsc --outDir dist --declaration false';
     const child = spawn(command, {
@@ -19,4 +30,20 @@ export async function runBuild(): Promise<void> {
       reject(e);
     });
   });
+}
+
+async function copyFiles() {
+  const dir = path.resolve(cwd, 'src');
+  const allFiles = globby
+    .sync(['**/*.*'], {
+      cwd: dir,
+    })
+    .filter((f) => !/\.[jt]s$/.test(f));
+  await Promise.all(
+    allFiles.map(async (f) => {
+      const p = path.resolve(dir, f);
+      const content = await fs.readFile(p, 'utf-8');
+      await fs.writeFile(path.resolve(distPath, f), content, 'utf-8');
+    })
+  );
 }
